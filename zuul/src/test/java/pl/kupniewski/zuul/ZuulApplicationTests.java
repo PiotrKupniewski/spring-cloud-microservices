ce20fb74878474ad11d7a92aae869b563be4dcb7
Piotr Kupniewski <pkupniewski.dev@gmail.com>
2020-12-11 00:27:59 +0100

    initil commit with some microservices and eureka service discovery

---
+++ zuul/src/test/java/pl/kupniewski/zuul/ZuulApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.kupniewski.zuul;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZuulApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
